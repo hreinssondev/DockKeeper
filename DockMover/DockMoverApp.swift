5d9feac04bc9e17d0578db37a3cd97e73159a488
hreinssondev <hreinssondev@gmail.com>
2026-05-29 18:25:26 +0200

    Initial Commit

---
+++ DockMover/DockMoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockMoverApp.swift
+//  DockMover
+//
+//  Created by H on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DockMoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
